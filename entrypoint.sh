d4b029935631a98f1a6bed53f86943038d27410a
PdcShoun <pdc.pachara@gmail.com>
2024-03-04 20:10:58 +0700

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-: ${PORT:=${PB_PORT:=8080}}
+PB_ARGS="serve --http=0.0.0.0:8080"
 
-exec /pb/pocketbase serve "--http=0.0.0.0:${PORT}"
+case "$1" in
+    -- | pocketbase)
+        shift
+        if [[ "$1" != "" ]] ; then
+            exec /pb/pocketbase "$@"
+        else
+            exec /pb/pocketbase "$@" $PB_ARGS
+        fi
+        ;;
+    -*)
+        exec /pb/pocketbase "$@"
+        ;;
+    *)
+        exec "$@"
+esac
 
 exit 1
