7d1f122ac2eca4b22e0062cf448e984caaefe486
PdcShoun <pdc.pachara@gmail.com>
2024-03-05 09:52:40 +0700

    [IMP]Dockerfile: change Git image

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,18 +3,19 @@
 set -e
 
 PB_ARGS="serve --http=0.0.0.0:8080"
+PB_BIN="/pb/pocketbase"
 
 case "$1" in
     -- | pocketbase)
         shift
         if [[ "$1" != "" ]] ; then
-            exec /pb/pocketbase "$@"
+            exec $PB_BIN "$@"
         else
-            exec /pb/pocketbase "$@" $PB_ARGS
+            exec $PB_BIN $PB_ARGS
         fi
         ;;
     -*)
-        exec /pb/pocketbase "$@"
+        exec $PB_BIN "$@"
         ;;
     *)
         exec "$@"
